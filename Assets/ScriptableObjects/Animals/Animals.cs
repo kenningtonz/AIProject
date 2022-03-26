c436884238455870f5bfb19b7e7fed125980407a
Dovah <halocodx47@hotmail.com>
2022-03-26 15:26:29 -0400

    shit was done

--- Assets/ScriptableObjects/Animals/Animals.cs
+++ Assets/ScriptableObjects/Animals/Animals.cs
@@ -10,7 +10,12 @@ public class Animals : MonoBehaviour
     private float moveTime = 10f;
     private float moveCounter;
     private TileBase currentTile;
+<<<<<<< Updated upstream
 
+=======
+    public GameObject senseRadius;
+    
+>>>>>>> Stashed changes
     public Sprite sprite;
     //public int[] attributes;
     public int m_fertility;
@@ -18,6 +23,8 @@ public class Animals : MonoBehaviour
     public int m_sense;
     public int m_belly;
     public int m_foreging;
+    public bool readytochild = false;
+    public bool nsff = false;
 
     private int weatherSpeed = 0;
     private int weatherSense = 0;
@@ -44,6 +51,7 @@ public class Animals : MonoBehaviour
     private void Update()
     {
         moveCounter -= Time.deltaTime;
+<<<<<<< Updated upstream
      if (moveCounter <= 0)
         {
             moveCounter = moveTime;
@@ -52,6 +60,17 @@ public class Animals : MonoBehaviour
 
         }
         transform.position += transform.up * Time.deltaTime * (m_speed + weatherSpeed);
+=======
+
+        //if (moveCounter <= 0)
+        //{
+        //    moveCounter = moveTime;
+        //    lookRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+        //    Debug.Log(lookRotation);
+        //}
+        //    transform.rotation = lookRotation;
+        //transform.position += transform.up * Time.deltaTime * (m_speed + weatherSpeed);
+>>>>>>> Stashed changes
 
         if (mapManager.getTileData(transform.position).tiles[0] != currentTile)
         {
@@ -72,7 +91,41 @@ public class Animals : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0f, 0f, newRotation);
             }
     }
+    public bool EatFoodOutsideBiome(Enums.Biomes biome, Enums.AnimalWarmth warmth, int speed, int sense, int food)
+    {
+        int threshold = 0;
+        //Threshold: Can/should I get this food?
+        //Is biome safe? 2
+        //What is my speed/sense ratio? Can I get the food fast? 4
+        //Am I starving? 6
+        //5-: don't eat, 6+ eat
+        if (!nsff)
+        {
+            threshold += 2;
+        }
 
+        if (speed > sense)
+        {
+            threshold += 4;
+        }
+
+        if (food <= 1)
+        {
+            threshold += 6;
+        }
+
+        if (threshold <= 5)
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool TakeRisks()
+    {
+        //Threshold: Am I hungry enough to take risks?
+        //2-: no risks, 3+ take risks
+        return false;
+    }
 
     public void BodyCheck(Enums.AnimalWarmth warmth, Enums.Weather weather)
     {
@@ -81,6 +134,7 @@ public class Animals : MonoBehaviour
         {
             weatherSense = 1;
             weatherSpeed = 1;
+            nsff = false;
         }
         else if ((warmth == Enums.AnimalWarmth.Fur && weather == Enums.Weather.Chilly)
                    || (warmth == Enums.AnimalWarmth.Smooth && weather == Enums.Weather.Cold)
@@ -89,24 +143,28 @@ public class Animals : MonoBehaviour
         {
             weatherSense = 0;
             weatherSpeed = 0;
+            nsff = false;
         }
         else if((warmth == Enums.AnimalWarmth.Smooth && weather == Enums.Weather.Warm)
             || (warmth == Enums.AnimalWarmth.Feathers && weather == Enums.Weather.Chilly))
         {
             weatherSpeed = -1;
             weatherSense = 0;
+            nsff = true;
         }
         else if ((warmth == Enums.AnimalWarmth.Feathers && weather == Enums.Weather.Warm)
           || (warmth == Enums.AnimalWarmth.Smooth && weather == Enums.Weather.Chilly))
         {
             weatherSpeed = 1;
             weatherSense = 0;
+            nsff = false;
         }
         else
         {
             Debug.Log(warmth);
             Debug.Log(weather);
             Destroy(gameObject);
+            nsff = true;
         }
     }
 }
--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class AnimalSpawner : MonoBehaviour
 {
     public GameObject prefab;
+<<<<<<< Updated upstream
+=======
+    private MapManager mapManager;
+    public List<int> a_fertility;
+    public List<int> a_speed;
+    public List<int> a_belly;
+    public List<int> a_sense;
+    public List<int> a_foraging;
+    public List<int> a_warmth;
+    public List<GameObject> livinganimals;
+    public int fertval;
+    public int speedval;
+    public int bellyval;
+    public int senseval;
+    public int forgval;
+    public int warmval;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +38,26 @@ public class AnimalSpawner : MonoBehaviour
 
             GameObject animal =  Instantiate(prefab, mousePos, Quaternion.identity);
             Debug.Log("spawn");
+<<<<<<< Updated upstream
             animal.GetComponent<Animals>().init(Random.Range(1,10), Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10),Enums.AnimalWarmth.Feathers);
 
 
+=======
+            fertval = Random.Range(1, 10);
+            bellyval = Random.Range(1, 10);
+            senseval = 2;
+            speedval = Random.Range(1, 10);
+            forgval = Random.Range(1, 10);
+            warmval = Random.Range(0, 3);
+            animal.GetComponent<Animals>().init(fertval, speedval, senseval, bellyval, forgval, warmval);
+            a_fertility.Add(fertval);
+            a_belly.Add(bellyval);
+            a_sense.Add(senseval);
+            a_speed.Add(speedval);
+            a_foraging.Add(forgval);
+            a_warmth.Add(warmval);
+            livinganimals.Add(animal);
+>>>>>>> Stashed changes
         }
     }
 }
